8c50a302444bfe66b133ebc3f728c933638cf8da
Lynn Jarvis <leadedge@internode.on.net>
2020-05-11 14:58:41 +0930

    Add files via upload

---
+++ getDownLoads.php
@@ -0,0 +1,30 @@
+<?php
+/*
+    Modified version of the file from GUP.
+
+    GUP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GUP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with GUP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+echo 
+"<?xml version=\"1.0\"?>
+<SpoutToNDI>
+	<Version>2.005</Version>
+	<Location>https://github.com/leadedge/leadedge.github.io/raw/downloads/SpoutToNDI_2005.zip</Location>
+</SpoutToNDI>
+<Spout>
+	<Version>2.006</Version>
+	<Location>https://github.com/leadedge/leadedge.github.io/raw/downloads/Spout_2006_update-2a.zip</Location>
+</Spout>"
+;
+
+?>
